710617c161f17744aafb355834238f820215462b
ka1sser <serenokaisser.dev@gmail.com>
2026-06-12 15:41:42 +0800

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
