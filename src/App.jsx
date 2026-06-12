4af043aebf241540f2c611f92e349bf69a6e7ebe
ka1sser <serenokaisser.dev@gmail.com>
2026-06-12 16:10:59 +0800

    Configured color design system and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import { gsap } from "gsap";
 
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-primary-300">
+      <p class="font-primary font-thin">Thin text</p>
+
+      <p class="font-primary font-normal">Regular text</p>
+
+      <p class="font-primary font-bold">Bold text</p>
+    </div>
+  );
 };
 
 export default App;
